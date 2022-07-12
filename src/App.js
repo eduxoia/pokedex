65f800a3d99b3cb4d7c004c57b37c5cc005234ad
Eduardo Scheuer <83767611+eduxoia@users.noreply.github.com>
2022-07-11 23:38:04 -0300

    Saying Hello World!
    Changing the introduction text.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World! My name is Eduardo! 
+        </p>
+        <p>
+          :D
         </p>
         <a
           className="App-link"
